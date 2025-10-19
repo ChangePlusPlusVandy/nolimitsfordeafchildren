1851dfb61e3ec4eac204a06ce95bfb9120c85d58
Ryan McCauley <ryanmcly@gmail.com>
2025-10-19 11:58:44 -0500

    chore: use correct postgres version

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,4 +1,4 @@
 import * as schema from "./schema";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 export const db = drizzle(process.env.POSTGRES_URI!, { schema });
